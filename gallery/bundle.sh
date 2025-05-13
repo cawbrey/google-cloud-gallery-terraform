ffcac160bad9466c83afac9af4b12c2268ccb657
Carter Awbrey <carterawb@gmail.com>
2025-05-13 10:18:52 -0500

    updated to use private vpc connection for cloudsql

--- gallery/bundle.sh
+++ gallery/bundle.sh
@@ -16,7 +16,7 @@ echo "Files have been restored inside the 'gallery' directory."
 
 # Update package list and install dependencies
 sudo apt-get update
-sudo apt-get install -yq python3-pip python3-venv python3-flask
+sudo apt-get install -yq python3-pip python3-venv
 
 
 # Run the flask app under a venv
@@ -27,6 +27,7 @@ pip install --upgrade pip
 pip install -r ./requirements.txt
 export FLASK_APP="main.py"
 export FLASK_RUN_PORT=80
+export FLASK_RUN_HOST="0.0.0.0"
 flask run 
 EOF
 
